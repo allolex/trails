940797328e20e44573eb705d0cad464b9fdc6dc4
Damon Davison <damon@curatur.com>
2020-03-02 10:01:59 -0500

    Use ES6 syntax for all components

--- components/trails.js
+++ components/trails.js
@@ -1,39 +1,47 @@
-function TrailTitle(props) {
-  return (
-    <h3>{props.value}</h3>
-  )
+class TrailTitle extends React.Component {
+  render() {
+    return(
+      <h3>{this.props.value}</h3>
+    )
+  }
 }
 
-function TrailDescription(props) {
-  return (
-    <p>
-      {props.value}
-    </p>
-  )
+class TrailDescription extends React.Component {
+  render() {
+    return (
+      <p>
+        {this.props.value}
+      </p>
+    )
+  }
 }
 
-function Trail(props) {
-  return (
-    [
-      <TrailTitle value={props.value} />,
-      <TrailDescription value={props.description} />
-    ]
-  )
+class Trail extends React.Component {
+  render() {
+    return (
+      [
+        <TrailTitle value={this.props.value} />,
+        <TrailDescription value={this.props.description} />
+      ]
+    )
+  }
 }
 
-function TrailCollection(props) {
-  const trails = props.trails;
+class TrailCollection extends React.Component {
+  render() {
+    const trails = this.props.trails;
 
-  return (
-    [
-      <h1>Trail Collection</h1>,
-      trails.map((trail) => {
-        return (
-          <Trail key={trail.id} value={trail.name} description={trail.description} />
-        )
-      })
-    ]
-  )
+    return (
+      [
+        <h1>Trail Collection</h1>,
+        trails.map((trail) => {
+          return (
+            <Trail key={trail.id} value={trail.name} description={trail.description} />
+          )
+        })
+      ]
+    )
+  }
 }
 
 export default TrailCollection;
